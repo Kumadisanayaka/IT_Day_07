2a7a7162f65fc1f118b4087093ad10bb869d9804
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-18 14:39:56 +0530

    Refactor: Uncomment toSorted function and add array mapping example for employee salaries

--- app.js
+++ app.js
@@ -67,15 +67,27 @@ console.log(sample("Hiiiii"));
 let sample = textValue => textValue;
 
 console.log(sample("Hi2"));
-*/
+
 
 //for Each Loop
 
 //products.forEach(product => console.log(product));
+
+//toSorted Function
 
 const letterList = ["F","A","C","B","D","E"];
 
 let sortArr = letterList.toSorted();
 
 console.log(sortArr);
+
+*/
+
+//Array mapping
+
+const employeeSalary = [75000,25000,50500,65000,81000,36000];
+
+let newSalary = employeeSalary.map(salary => salary * 2);
+
+console.log(newSalary);
 
